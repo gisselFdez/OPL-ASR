7d62aa53778a983fe5300f5afbe0796a537f7767
giss04 <gisselta04@hotmail.com>
2016-01-14 15:39:04 +0100

    style(Summary)

--- src/main/java/util/Summary.java
+++ src/main/java/util/Summary.java
@@ -1,20 +1,9 @@
 package util;
 
-import java.util.HashMap;
-import java.util.List;
-
 public class Summary {
-	private static int buggedTests=0;
 	private static int repairedTests=0;
 	
-	public static int getBuggedTests() {
-		return buggedTests;
-	}
-	
-	public static void addBuggedTest() {
-		Summary.buggedTests ++;
-	}
-	
+		
 	public static int getRepairedTests() {
 		return repairedTests;
 	}
